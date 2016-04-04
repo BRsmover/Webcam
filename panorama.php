9e93919b8c98aae607343f959f91094844d5a5cc
Benjamin Jenni <benjamin.jenni@secacon.com>
2016-04-04 15:27:20 +0200

    Merging images together

--- panorama.php
+++ panorama.php
@@ -50,16 +50,34 @@ header("Location: http://" . $ip . "/cgi-bin/camtrl.cgi?move=left");
 header("Location: http://" . $ip . "/cgi-bin/camtrl.cgi?move=left");
 
 // Loop to move right, make a picture, save it to temp and repeat that four times
-for($i = 1, $i <= 4, $i++) {
-    $i = file_put_contents("images/temp/" . $i, fopen("http://" . $ip . "/cgi-bin/video.jpg", "r");
+for($i = 1; $i <= 4; $i++) {
+    $i = file_put_contents("images/temp/" . $i, fopen("http://" . $ip . "/cgi-bin/video.jpg", "r"));
     header("Location: http://" . $ip . "/cgi-bin/camtrl.cgi?move=right");
 }
 
 // ---------------------------- Step 3 ----------------------------
 // Put them together with GD Library
+$first = imagecreatefromjpeg("images/temp/1.jpg");
+$second = imagecreatefromjpeg("images/temp/2.jpg");
+// Copy second onto first 100px from left side, on bottom, at beginning of second image, merge gradient is 50
+imagecopymerge($first, $second, 100, 0, 0, 0, 150, 150, 50);
 
+// Copy third onto already merged image
+$third = imagecreatefromjpeg("images/temp/3.jpg");
+imagecopymerge($first, $third, 200, 0, 0, 0, 150, 150, 50);
+
+// Copy fourth onto already merged image
+$fourth = imagecreatefromjpeg("images/temp/4.jpg");
+imagecopymerge($first, $fourth, 300, 0, 0, 0, 150, 150, 50);
+
+// Delete the temporary files
+for($i = 1; $i <= 4; $i++) {
+    unlink("images/temp/" . $i . ".jpg");
+}
 
 // ---------------------------- Step 4 ----------------------------
 // Save them into above specified path - Format: "panorama-webcam_d-m-Y_h-i.png" and return to homepage
+imagejpeg($first, "images/" . $day . "/" . $hour . "/panorama-webcam_" . $day . "-" . $hour . "-" . $minute);
 
+// All resources get destroyed automatically when the script ends
 ?>
