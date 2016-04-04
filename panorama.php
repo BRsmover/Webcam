001206f0e432c8b4d7fccfc4392bafc3d02416dd
Benjamin Jenni <benjamin.jenni@secacon.com>
2016-04-04 11:33:20 +0200

    Get date

--- panorama.php
+++ panorama.php
@@ -3,10 +3,20 @@
 
 // Get date, check if folder for day exists (create if not), check if folder for hour exists (create if not) and set
 // this one as path
+$date = date('d-m-Y-h-i');
+echo $date;
+
 
 // Shoot photos with webcam
+// turn to the far left
+
+// loop to move right, make a picture and repeat that
+
+
 
 // Put them together with GD Library
+
+
 
 // Save them into above specified path
 
